566ee91f902ce131ab1bf0c732916729b2038cdf
William <wwallas@gmail.com>
2022-12-22 09:20:33 -0800

    Add project files.

---
+++ AuthenthicationApp/Data/Migrations/20221221232350_Createdb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenthicationApp.Data.Migrations
+{
+    public partial class Createdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
